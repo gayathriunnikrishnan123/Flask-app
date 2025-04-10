9bc94c1bdb1d110934f35eac43fcd0c7edcba0c1
gayathriunnikrishnan123 <gayathriunnikrishnan000@gmail.com>
2025-04-10 22:49:51 +0530

    post api with validation

--- app.py
+++ app.py
@@ -4,14 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 # Initialize SQLAlchemy  12345......
 db = SQLAlchemy()
 
-# Define models
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f"<User {self.username}>"
 
 class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,14 +29,35 @@ def create_app():
     with app.app_context():
         db.create_all()
     
-    # Define a route for the root URL.............
-    @app.route('/')
-    def home():
-        return "Welcome to the Flask App!"
     
     @app.route('/add_car', methods=['POST'])
-    def add_car():
+    def items():
+        print(request)
+        print(request.json)
         data = request.json
+
+        if not "price" in data:
+            return jsonify({"message": "price not given"})
+        if not "name" in data:
+            return jsonify({"message": "name not given"})
+        if not "fuel" in data:
+            return jsonify({"message": "fuel not given"})
+        if not "seat_nu" in data:
+            return jsonify({"message": "seat number not given"})
+        
+        if not isinstance(data['seat_nu'],int):
+             return jsonify({"message": "seat number not integer"})
+        if not isinstance(data['price'],int):
+             return jsonify({"message": "price not integer"})
+        if not isinstance(data['fuel'],str):
+             return jsonify({"message": "fuel not string"})
+        if not isinstance(data['name'],str):
+             return jsonify({"message": "name not string"})
+        
+        if Car.query.filter_by(name=data['name']):
+            return jsonify({"message": "car whith this name already existing"})
+        
+
         new_car = Car(name=data['name'], fuel=data['fuel'], seat_nu=data['seat_nu'], price=data['price'])
         db.session.add(new_car)
         db.session.commit()
